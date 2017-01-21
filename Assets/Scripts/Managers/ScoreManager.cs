4f11387604f4ebb6aef07ca4213c6dfe2ed5716b
Tomasz Rozwadowski <olsen6@gmail.com>
2017-01-21 18:45:30 +0100

    UI

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int woodLeft;
+
+
+    Text text;
+
+
+    void Awake ()
+    {
+        text = GetComponent <Text> ();
+		woodLeft = 0;
+    }
+
+
+    void Update ()
+    {
+		text.text = " " + woodLeft;
+    }
+}
