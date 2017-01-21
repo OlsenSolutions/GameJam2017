87a42c8359e1007c431a8025ca217bc09c104676
iabulko <marekblaz@gmail.com>
2017-01-21 12:02:11 +0100

    Piatek noc

---
+++ Assets/Scripts/Buildings/Ship.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+	public int planksAddedNumber = 0;
+	private int lastPlanksAddedNumber = 0;
+
+	private int maxPlanksNumber;
+
+	private List<GameObject> addedPlanksGOs = new List<GameObject>();
+	private List<GameObject> notAddedPlanksGOs = new List<GameObject>();
+
+	private Transform planksParent;
+
+
+	void Awake()
+	{
+		planksParent = transform.Find("Planks");
+		maxPlanksNumber = planksParent.childCount;
+		lastPlanksAddedNumber = planksAddedNumber;
+
+		for (int i = 0; i < maxPlanksNumber; i++)
+		{
+			notAddedPlanksGOs.Add(planksParent.GetChild(i).gameObject);
+			planksParent.GetChild(i).gameObject.SetActive(false);
+		}
+
+	}
+
+	void Update()
+	{
+		planksAddedNumber = Mathf.Clamp(planksAddedNumber, 0, maxPlanksNumber);
+
+		if (planksAddedNumber > lastPlanksAddedNumber)
+		{
+			for (int i = 0; i < planksAddedNumber - lastPlanksAddedNumber; i++)
+			{
+				int id = Random.Range(0, notAddedPlanksGOs.Count);
+				addedPlanksGOs.Add(notAddedPlanksGOs[id]);
+				notAddedPlanksGOs[id].SetActive(true);
+				notAddedPlanksGOs.RemoveAt(id);
+			}
+		}
+		else if (planksAddedNumber < lastPlanksAddedNumber)
+		{
+			for (int i = 0; i < lastPlanksAddedNumber - planksAddedNumber; i++)
+			{
+				int id = Random.Range(0, addedPlanksGOs.Count);
+				notAddedPlanksGOs.Add(addedPlanksGOs[id]);
+				addedPlanksGOs[id].SetActive(false);
+				addedPlanksGOs.RemoveAt(id);
+			}
+		}
+
+		lastPlanksAddedNumber = planksAddedNumber;
+	}
+}
