a66ea66f5b981f40297897f1518b0de26d98acfb
Tomasz Rozwadowski <olsen6@gmail.com>
2017-01-21 22:56:26 +0100

    working menus

---
+++ Assets/Scripts/Managers/WaveTimeManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WaveTimeManager : MonoBehaviour
+{
+    public static float waveTimer;
+
+
+    Text text;
+
+
+    void Awake ()
+    {
+        text = GetComponent <Text> ();
+		waveTimer = 0;
+    }
+
+
+    void Update ()
+    {
+		text.text = " " + waveTimer;
+    }
+}
